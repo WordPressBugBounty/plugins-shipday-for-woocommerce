669d8e7ab3ad9bba6bb4dbc36574bb1dd4ce1fc1
Sean Murphy <sean@iamseanmurphy.com>
2025-07-30 07:19:15 +0000

    1.8.8

--- shipday-for-woocommerce/dispatch_post/post_fun.php
+++ shipday-for-woocommerce/dispatch_post/post_fun.php
@@ -15,15 +15,40 @@ function shipday_post_orders(array $payloads) {
 			if ($response['http_code'] != 200) {
                 shipday_logger('error', 'Post failed for API key: '.$api_key);
             }
-//			if ($shipday_debug_flag == true) shipday_post_order(
-//                array(
-//                    'payload' => $payload,
-//                    'response' => $response
-//                ),
-//                $api_key, get_shipday_debug_api_url()
-//            );
+			if ($shipday_debug_flag == true) shipday_post_order(
+                array(
+                    'payload' => $payload,
+                    'response' => $response
+                ),
+                $api_key, get_shipday_debug_api_url()
+            );
 		}
 	}
+    return $success;
+}
+
+function shipday_post_pickup_orders(array $payloads) {
+    global $shipday_debug_flag;
+    $success = false;
+    shipday_logger('INFO', 'Pickup order payload: ' . json_encode($payloads));
+
+    foreach ($payloads as $api_key => $payload_array) {
+        $api_key = trim($api_key);
+        foreach ($payload_array as $payload){
+            $response = shipday_post_order($payload, $api_key, get_shipday_pickup_api_url());
+            $success |= ($response['http_code'] == 200);
+            if ($response['http_code'] != 200) {
+                shipday_logger('error', 'Pickup order post failed for API key: '.$api_key);
+            }
+            if ($shipday_debug_flag == true) shipday_post_order(
+                array(
+                    'payload' => $payload,
+                    'response' => $response
+                ),
+                $api_key, get_shipday_debug_api_url()
+            );
+        }
+    }
     return $success;
 }
 
